af55c2b8ca8d75f790f9aafdf7491932c27a9339
Cristian Hancu <77895092+cristianhancuu@users.noreply.github.com>
2025-05-10 22:53:42 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const buttonYes = document.getElementById('yes');
+const image = document.getElementById('image');
+const buttonno= document.getElementById('no');
+
+  buttonYes.addEventListener('touchstart', () => {
+    const maxX = window.innerWidth - buttonYes.offsetWidth;
+    const maxY = window.innerHeight - buttonYes.offsetHeight;
+
+    const randomX = Math.floor(Math.random() * maxX);
+    const randomY = Math.floor(Math.random() * maxY);
+
+    buttonYes.style.left = `${randomX}px`;
+    buttonYes.style.top = `${randomY}px`;
+  });
+    buttonYes.addEventListener('mouseenter', () => {
+    const maxX = window.innerWidth - buttonYes.offsetWidth;
+    const maxY = window.innerHeight - buttonYes.offsetHeight;
+
+    const randomX = Math.floor(Math.random() * maxX);
+    const randomY = Math.floor(Math.random() * maxY);
+
+    buttonYes.style.left = `${randomX}px`;
+    buttonYes.style.top = `${randomY}px`;
+  });
+  buttonno.addEventListener('click', ()=> {
+    buttonYes.style.visibility = 'hidden';
+    image.style.display = 'block';
+
+    
+  })
